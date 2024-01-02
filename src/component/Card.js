fedc197993c06a2f38d27e184d49df1a55a2fe4f
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-02 14:50:58 +0530

    almost done

--- src/component/Card.js
+++ src/component/Card.js
@@ -7,7 +7,7 @@ const Card=(Props)=>{
       <img src={Props.exercise.gifUrl} alt={Props.exercise.name} className="imgg" />
       <h2 className="ExeName">{Props.exercise.name}</h2>
       <p className="Exetarget">{Props.exercise.target}</p>
-      <p className="ExeName">{Props.exercise.bodyPart}</p>
+      <p className="Exeparts">{Props.exercise.bodyPart}</p>
     </div>
     )
 }
